847a229722f7ffcb961aa0bbbb7447419837cd8c
Jayram <Jayram.kumar@kaarya.com>
2014-09-02 15:47:18 +0530

    removed the dirty comment

--- NewFile.java
+++ NewFile.java
@@ -1,6 +1,5 @@
 public class NewFile {
 	// This is a new comment.
-			//	ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
 	public static final int p = 1;
 	//new comment in master
 }
