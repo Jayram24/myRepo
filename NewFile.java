19d25ddd0c1a10770492e3fea162bc25b8ec8692
Jayram <Jayram.kumar@kaarya.com>
2014-09-02 15:56:25 +0530

    special commit for int

--- NewFile.java
+++ NewFile.java
@@ -2,6 +2,7 @@ public class NewFile {
 	// This is a new comment.
 	public static final int p = 1;
 	//new comment in master
+	//special comment for int
 	public void comment1() {}
 	public void comment2() {}
 	//One more comment
