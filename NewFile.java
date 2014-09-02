0277e51a5db65644b015177f7349a1c76c93aba4
Jayram <Jayram.kumar@kaarya.com>
2014-09-02 15:21:37 +0530

    added a new final variable

--- NewFile.java
+++ NewFile.java
@@ -1,3 +1,4 @@
 public class NewFile {
 			//	ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+	public static final int p = 1;
 }
